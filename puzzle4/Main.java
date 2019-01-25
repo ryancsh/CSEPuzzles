522f8ae7007f0f46519d86caf914cd6e93d01990
Ryan Chan Shum Hang <rchanshu@scs.ryerson.ca>
2019-01-25 17:17:02 -0500

    update main.java

--- puzzle4/Main.java
+++ puzzle4/Main.java
@@ -1,6 +1,16 @@
 class Main{
   public static void main(String[] args){
     Main m = new Main();
-    System.out.println((int)'@');
+  }
+
+  static final String Filename = "input.txt";
+
+  Vector<Character> symbols;
+  Vector<Integer> numbers;
+
+  public Main(){
+    Scanner in = new Scanner(new File(Filename));
+
+    while(in.hasNext()){
   }
 }
